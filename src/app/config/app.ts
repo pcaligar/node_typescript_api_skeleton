2c577e7afbf52e8133359fd8b0f5b2ac7f7049e1
pcaligar <paulo.caligaris@gmail.com>
2022-03-04 21:56:55 -0300

    feat: add express and basic server

---
+++ src/app/config/app.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+// import setupMiddlewares from './setup-middlewares'
+// import setupRoutes from './setup-routes'
+
+const app = express();
+// setupMiddlewares(app)
+// setupRoutes(app)
+
+export default app;
