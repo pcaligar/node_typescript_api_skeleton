a048b132fe37d7c023d1f3b1087dc74636b5277e
pcaligar <paulo.caligaris@gmail.com>
2022-03-07 19:58:54 -0300

    feat: apply setupRoutes and add status route

--- src/app/config/app.ts
+++ src/app/config/app.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 import setupMiddlewares from './setupMiddlewares';
-// import setupRoutes from './setup-routes'
+import setupRoutes from './setupRoutes';
 
 const app = express();
 setupMiddlewares(app);
-// setupRoutes(app)
+setupRoutes(app);
 
 export default app;
