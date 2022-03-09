30f1e10e182427170d0ba04a64ce0befbecab5f7
pcaligar <paulo.caligaris@gmail.com>
2022-03-09 20:29:55 -0300

    sync interface controller, statusGetController and status route

--- src/app/config/routes/status.route.ts
+++ src/app/config/routes/status.route.ts
@@ -1,10 +1,7 @@
 import { Router, Request, Response } from 'express';
-// import StatusGetController from '../../controllers/StatusGetController'
+import StatusGetController from '../../controllers/StatusGetController';
 
 export default (router: Router): void => {
-  // const controller = new StatusGetController()
-  // router.get('/status', (req: Request, res: Response) => controller.run(req, res))
-  router.get('/status', (req: Request, res: Response) => {
-    res.status(200).send({ ok: 'ok' });
-  });
+  const controller = new StatusGetController();
+  router.get('/status', (req: Request, res: Response) => controller.run(req, res));
 };
