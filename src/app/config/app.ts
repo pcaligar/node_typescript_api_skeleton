0f2872509fd7836d54112479528bbe2710402f3b
pcaligar <paulo.caligaris@gmail.com>
2023-09-07 13:48:58 -0300

    feat: add security helmet package

--- src/app/config/app.ts
+++ src/app/config/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import helmet from "helmet";
 
 import bodyParser from "./middleware/bodyParser";
 import setupMiddleware from "./setupMiddleware";
@@ -6,6 +7,10 @@ import setupRoutes from "./setupRoutes";
 
 const app = express();
 app.use(bodyParser);
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({ action: "deny" }));
 setupRoutes(app);
 setupMiddleware(app);
 
